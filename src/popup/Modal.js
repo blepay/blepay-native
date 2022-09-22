7a188252972ea27c35bc81d8b0776b5204b14d12
ritsuyan <ritsu.yan@foxmail.com>
2022-09-22 11:09:18 +0800

    Update Modal.js

--- src/popup/Modal.js
+++ src/popup/Modal.js
@@ -10,10 +10,9 @@ import {
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
-
 export function ModalBox({ visible, containerStyle, children, close }) {
   const [showDialog, setShowDialog] = React.useState(visible);
-  console.log('show',showDialog)
+  console.log("show", showDialog);
   React.useEffect(() => {
     setShowDialog(visible);
   }, [visible]);
@@ -29,7 +28,7 @@ export function ModalBox({ visible, containerStyle, children, close }) {
         <TouchableHighlight
           style={containerStyle ? containerStyle : styles.dialogContainer}
         >
-          <View style={{height: '100%'}}>{children}</View>
+          <View style={{ height: "100%" }}>{children}</View>
         </TouchableHighlight>
       </TouchableOpacity>
     </Modal>
@@ -40,13 +39,19 @@ const styles = StyleSheet.create({
   dialogBackground: {
     flex: 1,
     backgroundColor: "rgba(18, 31, 0, 0.75)",
-    position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
   dialogContainer: {
     // pointerEvents: "none",r
-    backgroundColor: 'white',
+    backgroundColor: "white",
     width: 0.9 * windowWidth,
-    height: 612/844 * windowHeight,
+    height: (612 / 844) * windowHeight,
     backgroundColor: "white",
     paddingHorizontal: 20,
     paddingVertical: 20,
